ffa7de11e3fcc3d0cca41f0872edbafc6b7f04f5
ericgaspar <junk.eg@free.fr>
2021-01-29 09:11:44 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,9 +8,6 @@ YNH_PHP_VERSION="7.3"
 
 extra_php_dependencies="php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-tidy php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysql"
 
-# dependencies used by the app
-#pkg_dependencies="deb1 deb2 php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
