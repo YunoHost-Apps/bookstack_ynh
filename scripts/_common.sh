f00bbb83d980fc8409ab61bf3e5842711641fac5
ericgaspar <junk.eg@free.fr>
2021-01-29 09:13:23 +0100

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -21,7 +21,7 @@ extra_php_dependencies="php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-curl php
 #=================================================
 
 
-readonly YNH_DEFAULT_COMPOSER_VERSION=2.0.8
+readonly YNH_DEFAULT_COMPOSER_VERSION=1.10.17
 # Declare the actual composer version to use.
 # A packager willing to use another version of composer can override the variable into its _common.sh.
 YNH_COMPOSER_VERSION=${YNH_COMPOSER_VERSION:-$YNH_DEFAULT_COMPOSER_VERSION}
