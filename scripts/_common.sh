bf4862afb9b17cff154098e91937db796fb237d1
ericgaspar <junk.eg@free.fr>
2021-06-13 23:14:52 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,7 +8,7 @@ YNH_PHP_VERSION="7.3"
 
 extra_php_dependencies="php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-tidy php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-gd"
 
-YNH_COMPOSER_VERSION=2.0.14
+YNH_COMPOSER_VERSION=2.1.1
 
 #=================================================
 # PERSONAL HELPERS
